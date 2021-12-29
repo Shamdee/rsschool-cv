dff6dfb82992d284b20def3c29f8b4a589e279ef
Shamdee <shamdee@gmail.com>
2021-12-29 21:37:07 +0200

    refactor: optimization html and css

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function myFunction() {
-    let x =document.querySelector('div.topnav');
-    if (x.className === 'topnav') {
+    let x = document.querySelector('ul.nav-list');
+    if (x.className === 'nav-list') {
         x.className += ' responsive';
     } else {
-        x.className = 'topnav';
+        x.className = 'nav-list';
     }
 }
 
-const anchors = document.querySelectorAll('a[href*="#"]')
-for (let anchor of anchors) {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault()
+// const anchors = document.querySelectorAll('a[href*="#"]')
+// for (let anchor of anchors) {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault()
 
-        const blockID = anchor.getAttribute('href').substr(1)
+//         const blockID = anchor.getAttribute('href').substr(1)
 
-        document.getElementById(blockID).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-    })
-}
+//         document.getElementById(blockID).scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'start'
+//         })
+//     })
+// }
 
 hljs.initHighlightingOnLoad();
 
