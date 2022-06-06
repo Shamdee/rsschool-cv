3c13776ddd28563557ba2da9d008eada78cad591
Shamdee <shamdee@gmail.com>
2022-06-06 18:16:36 +0300

    fix: edit js file

--- index.js
+++ index.js
@@ -7,20 +7,6 @@ function myFunction() {
     }
 }
 
-// const anchors = document.querySelectorAll('a[href*="#"]')
-// for (let anchor of anchors) {
-//     anchor.addEventListener('click', function(e) {
-//         e.preventDefault()
 
-//         const blockID = anchor.getAttribute('href').substr(1)
-
-//         document.getElementById(blockID).scrollIntoView({
-//             behavior: 'smooth',
-//             block: 'start'
-//         })
-//     })
-// }
 
 hljs.initHighlightingOnLoad();
-
-console.log('My self-rating is 140 points')
